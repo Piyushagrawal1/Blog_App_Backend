64e844e09c0c137e153d01cbeab3fdeb39f2fbf3
Piyushagrawal1 <agarwalpiyush166@gmail.com>
2024-05-28 23:57:09 +0530

    correct Typo

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const expres = require('express')
-const app = expres()
-const blog = require('./routes/blog')
-const connectDb = require('./config/database')
 
+const express = require('express'); 
+const app = express(); 
+const blog = require('./routes/blog');
+const connectDb = require('./config/database');
 
-require('dotenv').config()
-const PORT = process.env.DATABASE_URL || 3000
+require('dotenv').config();
+const PORT = process.env.PORT || 3000; 
 
+// Middleware
+app.use(express.json()); 
 
-//middleware
-app.use(expres.json())
+// Routes
+app.use('/api/v1', blog);
 
+// Database
+connectDb();
 
-//routes
-app.use('/api/v1', blog)
-
-
-//database
-connectDb()
-
-//server start
+// Server start
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
